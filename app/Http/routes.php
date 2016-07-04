fc00bd4b1b295d9f869cfad4dce561e39287e6f5
Mehmet Uygun <mehmet.uygun@outlook.com>
2016-07-05 01:56:42 +0300

    Create laravel authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/helloworld', function () {
+    return 'helloworld!';
+});
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
