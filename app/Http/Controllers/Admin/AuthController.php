bfb4df30a053320aae34cdc3515d8738b25f18b8
Mehmet Uygun <mehmet.uygun@outlook.com>
2016-07-11 03:32:21 +0300

    Create auth controller

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use Auth;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function postLogin(Request $request) 
+    {
+    	$conditions = [
+    		'email' => $request->input('email'),
+    		'password' => $request->input('password'),
+    		'is_admin' => 1,
+    	];
+    	if (Auth::attempt($conditions)) {
+    		return redirect('admin/home');
+    	}
+    }
+}
